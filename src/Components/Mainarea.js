842a2404ec14cfa888633f41308279270dd5f649
Adarsh Pandey <mail.apandey2001@gmail.com>
2022-01-09 17:05:13 +0530

    Video 7 Changing card backgrounds based on selections

--- src/Components/Mainarea.js
+++ src/Components/Mainarea.js
@@ -7,6 +7,7 @@ export default function Mainarea(props) {
 
     const [cardArray, setCardArray] = useState([]);
     const [revealedCardsArray, setRevealedCardsArray] = useState([]);
+    const [cardsColArr, setcardsColArr] = useState([]);
     const [selection1, setSelection1] = useState(null);
     const [selection2, setSelection2] = useState(null);
     const [totalFlips, setTotalFlips] = useState(0);
@@ -18,25 +19,24 @@ export default function Mainarea(props) {
         for (let i = 0; i < difficulty * difficulty; i++) {
             tempCardArray.push(i);
         }
-        console.log(tempCardArray);
         let shuffledArray = [];
         let revealedCards = [];
+        let tempCardColArr = [];
         for (let i = 0; i < difficulty * difficulty; i++) {
             const randomIndex = Math.floor(Math.random() * tempCardArray.length);
             shuffledArray.push(tempCardArray[randomIndex]);
             tempCardArray.splice(randomIndex, 1);
             revealedCards.push(false);
+            tempCardColArr.push("light")
         }
-        console.log(shuffledArray);
         setCardArray(shuffledArray);
         setRevealedCardsArray(revealedCards);
-        console.log(revealedCards);
+        setcardsColArr(tempCardColArr);
         // eslint-disable-next-line
     }, [])
 
     const handleSelection = (value) => {
         if (canSelect) {
-            console.log("Clicked", value);
             if (selection1 === null) {
                 setSelection1(value);
                 let newVisibleCardsArray = revealedCardsArray;
@@ -65,17 +65,38 @@ export default function Mainarea(props) {
     }
 
     useEffect(() => {
+        if (selection1 !== null) {
+            let newCardColArr = {...cardsColArr};
+            newCardColArr[selection1] = "info";
+            setcardsColArr(newCardColArr);
+        }
+    }, [selection1]);
+
+    useEffect(() => {
         if (selection1 !== null && selection2 !== null) {
             setTotalFlips(prevState => prevState+1);
+            let newCardColArr = {...cardsColArr};
+            newCardColArr[selection2] = "info";
+            setcardsColArr(newCardColArr);
             if (selection1 % 8 === selection2 % 8) {
-                console.log("Same");
+                newCardColArr = {...cardsColArr};
+                newCardColArr[selection1] = "success";
+                newCardColArr[selection2] = "success";
+                setcardsColArr(newCardColArr);
                 handleReset();
                 setflipsRemaining(prevState => prevState-1);
             }
             else {
-                console.log("Not Same");
                 setCanSelect(false);
+                newCardColArr = {...cardsColArr};
+                newCardColArr[selection1] = "danger";
+                newCardColArr[selection2] = "danger";
+                setcardsColArr(newCardColArr);
                 setTimeout(() => {
+                    newCardColArr = {...cardsColArr};
+                    newCardColArr[selection1] = "light";
+                    newCardColArr[selection2] = "light";
+                    setcardsColArr(newCardColArr);
                     hideCards();
                     handleReset();
                     setCanSelect(true);
@@ -93,7 +114,7 @@ export default function Mainarea(props) {
             <h3>flipsRemaining: {flipsRemaining}</h3>
             <div className="row">
                 {cardArray.map((element) => {
-                    return <Card key={element} visible={false} value={element} visible={revealedCardsArray[element]} handleSelection={handleSelection} />
+                    return <Card key={element} value={element} visible={revealedCardsArray[element]} bgCol={cardsColArr[element]} handleSelection={handleSelection} />
                 })}
             </div>
         </div>
